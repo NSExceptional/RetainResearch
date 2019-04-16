ec5b62c2f725ada5f1550ab939b81a7b3b22acb4
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-04-16 18:25:03 +0300

    Initial Commit

---
+++ RetainTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RetainTests
+//
+//  Created by Alexander Leontev on 16/04/2019.
+//  Copyright © 2019 Alexander Leontev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RetainTests/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RetainTests
+//
+//  Created by Alexander Leontev on 16/04/2019.
+//  Copyright © 2019 Alexander Leontev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
