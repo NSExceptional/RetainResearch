902d2f7b888a61ef770c3d30273d3da56b34e5d7
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-04-16 18:43:10 +0300

    Initial commit

--- RetainTests/ViewController.m
+++ RetainTests/ViewController.m
@@ -8,15 +8,59 @@
 
 #import "ViewController.h"
 
+@interface DeallocWatchdog: NSObject
+@end
+
+@implementation DeallocWatchdog
+
+- (void)dealloc {
+    NSLog(@"%@", @"I was deallocated, woof");
+}
+
+@end
+
 @interface ViewController ()
+
+@property (nonatomic) DeallocWatchdog *watchdog;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)testBridgeTransfer:(id)sender {
+    SEL watchdogSelector = @selector(watchdog);
+    NSInvocation *getWatchdogInvocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:watchdogSelector]];
+    getWatchdogInvocation.target = self;
+    getWatchdogInvocation.selector = watchdogSelector;
+    
+    [getWatchdogInvocation invoke];
+    
+    id returnObject = nil;
+
+    const void *objectReturnedFromMethod = nil;
+    [getWatchdogInvocation getReturnValue:&objectReturnedFromMethod];
+    returnObject = (__bridge_transfer id)objectReturnedFromMethod;
+}
+
+- (IBAction)testBridge:(id)sender {
+    SEL watchdogSelector = @selector(watchdog);
+    NSInvocation *getWatchdogInvocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:watchdogSelector]];
+    getWatchdogInvocation.target = self;
+    getWatchdogInvocation.selector = watchdogSelector;
+    
+    [getWatchdogInvocation invoke];
+    
+    id returnObject = nil;
+    
+    const void *objectReturnedFromMethod = nil;
+    [getWatchdogInvocation getReturnValue:&objectReturnedFromMethod];
+    returnObject = (__bridge id)objectReturnedFromMethod;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.watchdog = [DeallocWatchdog new];
 }
 
 
